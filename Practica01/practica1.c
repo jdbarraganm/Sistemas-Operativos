6c53ae5a536611a0713b04524242d99bb0b71329
jdbarraganm <jdbarraganm@unal.edu.co>
2018-09-22 12:47:58 -0500

    Update practica1.c

--- Practica01/practica1.c
+++ Practica01/practica1.c
@@ -5,6 +5,7 @@
 #define max 1000
 
 struct dogType{
+    struct pair_int *record;
     char Name[32];
     char Type[32];
     int Age;
@@ -15,18 +16,11 @@ struct dogType{
     struct dogType *next;
 };
 
-struct node{
-    int key;
-    struct dogType *value;
-    struct node *next;
+struct pair_int{
+    int first;
+    int second;
 };
 
-struct arrayitem{
-    struct node *head;
-    struct node *tail;
-};
-
-struct arrayitem *array;
 int countRecords = 0;
 struct dogType *hash_table[max];
 struct dogType *next=NULL;
@@ -36,15 +30,14 @@ int hash_function(char a[32]){
   int hash = 0;
   for (int i = 0; i < 32; i++) {
     printf("%i\t",a[i]);
+    if(a[i]>=65&&a[i]<=90){
+        a[i]+=32;
+    }
     hash = (31 * hash + a[i])%max;
   }
   printf("\n");
   return hash;
 }
-
-struct node* get_element(struct node *list, int find_index);
-void rehash();
-void init_array();
 
 struct dogType *createDog(){
   struct dogType *newDog = malloc(sizeof(struct dogType));
@@ -52,6 +45,7 @@ struct dogType *createDog(){
     printf("Por favor ingrese los datos pedidos a continuación\n"
            "ingrese nombre:\n"
            "Cuando haya terminado presione enter\n");
+    memset(newDog->Name,32,32);
     scanf("%s", newDog->Name);
     printf("ingrese especie:\n"
            "Cuando haya terminado presione enter\n");
@@ -76,8 +70,6 @@ struct dogType *createDog(){
   return newDog;
 }
 
-
-
 void loadDog(void *dog){
     struct dogType *newDog = dog;
     char n[32];
@@ -85,20 +77,29 @@ void loadDog(void *dog){
     memcpy(prueba,newDog->Name,32);
     int address = hash_function(n);
     struct dogType *pointer = hash_table[address];
-
+    int addressSec = 0;
     if(pointer==NULL){
         printf("entro al null\n");
         hash_table[address] = newDog;
+        newDog->record = malloc(sizeof(struct pair_int));
+        newDog->record->first = address;
+        newDog->record->second = addressSec;
     }else {
       printf("no es null\n");
+      pointer=hash_table[address];
+      addressSec+=1;
       while (pointer->next != NULL) {
-        printf("+1\n");
         pointer = pointer->next;
+        addressSec+=1;
       }
       pointer->next = malloc (sizeof(struct dogType));
       pointer->next = newDog;
-      /*pointer->next = NULL;*/
+      newDog->record = malloc(sizeof(struct pair_int));
+      newDog->record->first = address;
+      newDog->record->second = addressSec;
     }
+    printf("dirección 1: %d\n", address);
+    printf("dirección 2: %d\n", addressSec);
     printf("registro hecho\n");
     countRecords++;
     preMenu();
@@ -107,33 +108,35 @@ void loadDog(void *dog){
 void seeRecord(){
     printf("Cantidad de registros:\t"
            "%d\n",countRecords);
-    printf("Por favor ingrese el numero de registro a ver\n");
+    printf("Por favor ingrese la dirección 1 de registro a ver\n");
     int record;
     scanf("%i",&record);
-    if(hash_table[record]==NULL){
+    printf("Por favor ingrese la dirección 2 de registro a ver\n");
+    int record2;
+    struct dogType *pointer = hash_table[record];
+    scanf("%i",&record2);
+    if(pointer==NULL){
         printf("paila no hay nothing\n");
     }else{
-        system("gedit dataDogs.dat");
-        printf("hay algo\n");
-        char n[32];
-        char t[32];
-        memcpy(n,hash_table[record]->Name,32);
-        memcpy(t,hash_table[record]->Type,32);
-
-        printf("nombre:  %s\n",n);
-        printf("tipo: %s\n",t);
-        printf("Edad: %i\n",hash_table[record]->Age);
-        struct dogType *dogNext;
-        if(hash_table[record]->next!=NULL){
-            dogNext = hash_table[record]->next;
-            printf("hay algo despues del next\n");
-            char n2[32];
-            char t2[32];
-            memcpy(n2,dogNext->Name,32);
-            memcpy(t2,dogNext->Type,32);
-            printf("nombre:  %s\n",n2);
-            printf("tipo: %s\n",t2);
-            printf("Edad: %i\n",dogNext->Age);
+        int pos=0;
+        while (pointer->next!=NULL && pos!=record2) {
+          pointer=pointer->next;
+          pos++;
+        }
+        if (pos==record2) {
+          system("gedit dataDogs.dat");
+          printf("hay algo\n");
+          char n[32];
+          char t[32];
+          memcpy(n,pointer->Name,32);
+          memcpy(t,pointer->Type,32);
+          printf("nombre:  %s\n",n);
+          printf("tipo: %s\n",t);
+          printf("Edad: %i\n",pointer->Age);
+          printf("Record: %i\n",pointer->record->first);
+          printf("Record: %i\n",pointer->record->second);
+        }else{
+          printf("Registro no existente\n");
         }
     }
     preMenu();
@@ -154,7 +157,7 @@ void searchRecord(){
     printf("%d\n",hash_function(prueba));
     printf("Por favor digite el nombre de su mascota\n"
            "Seguida la tecla ENTER\n");
-    memset(n,0,32);
+    memset(n,32,32);
     scanf("%s",n);
     int addres = hash_function(n);
     printf("%s\n",n);
@@ -238,4 +241,3 @@ int main(){
     menu();
     return 0;
 }
-
